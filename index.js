a14004a234e0c2d44beef05132063c512eb7c0fc
DynoOp2004 <95514648+DynoOp2004@users.noreply.github.com>
2026-01-17 10:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("ready", async () => {
     .setAssetsLargeImage(
       "https://media.discordapp.net/attachments/1437376805353947139/1461941693594079404/monokuma-discord-logo-512x512.png?ex=696c62cd&is=696b114d&hm=67bdcee5d4129abf62158600ecc8e000d967b4b69c786b909c3ba45eae3544e6&=&format=webp&quality=lossless&width=768&height=768",
     ) //You can put links in tenor or discord and etc.
-    .setAssetsLargeText("We are the sinners.") //Text when you hover the Large image
+    .setAssetsLargeText("We are the secrets.") //Text when you hover the Large image
     .setAssetsSmallImage(
       "https://cdn.discordapp.com/attachments/1437376805353947139/1460775258213847155/1219350987249025045.gif?ex=696c18f9&is=696ac779&hm=0d16a0d4b45a2471f704f86d2224c632811083425f0aed5df0b5c95852417231&",
     ) //You can put links in tenor or discord and etc.
